8228d5ad0f4a095eafdba30f3b4f8cf1fcc2965f
Seth Dandridge <seth.dandridge@addepar.com>
2024-05-04 23:38:42 -0400

    Unfuck it

--- scripts/load_households.py
+++ scripts/load_households.py
@@ -1,11 +1,11 @@
 import csv
 import sqlite3
 
-with open("../instance/households.csv") as f:
+with open("instance/households.csv") as f:
     reader = csv.DictReader(f)
     households = list(reader)
 
-conn = sqlite3.connect("../instance/wedding_website.sqlite")
+conn = sqlite3.connect("instance/wedding_website.sqlite")
 c = conn.cursor()
 for household in households:
     household_id = int(household["ID"])
@@ -13,7 +13,7 @@ for household in households:
     is_invited_dinner = household["is_invited_dinner"] == "TRUE"
     is_invited_brunch = household["is_invited_brunch"] == "TRUE"
     c.execute(
-        "INSERT INTO household (guest_id, cute_name, is_invited_dinner, is_invited_brunch) VALUES (?, ?, ?, ?)",
+        "INSERT INTO household (id, cute_name, is_invited_dinner, is_invited_brunch) VALUES (?, ?, ?, ?)",
         (household_id, cute_name, is_invited_dinner, is_invited_brunch),
     )
 conn.commit()
